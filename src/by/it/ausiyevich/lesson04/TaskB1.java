afea20a4504dd3c9a21a073dd4aa39c704937b8b
Ausiyevich <avsievich69@gmail.com>
2018-06-12 19:03:32 +0300

    Lesson4 TaskB1 and B2 completed

--- src/by/it/ausiyevich/lesson04/TaskB1.java
+++ src/by/it/ausiyevich/lesson04/TaskB1.java
@@ -1,5 +1,7 @@
 package by.it.ausiyevich.lesson04;
 
+import java.util.Scanner;
+
 /*
 Определите количество дней в году
 
@@ -26,5 +28,27 @@ package by.it.ausiyevich.lesson04;
 
 */
 public class TaskB1 {
+    public static void main(String[] args) throws Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
 
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                System.out.println("количество дней в году: 366");
+            }
+            else {
+                System.out.println("количество дней в году: 365");
+            }
+        }
+        else {
+            if (year % 4 == 0) {
+                System.out.println("количество дней в году: 366");
+            }
+            else {
+                System.out.println("количество дней в году: 365");
+            }
+        }
+
+    }
 }
--- src/by/it/ausiyevich/lesson04/TaskB2.java
+++ src/by/it/ausiyevich/lesson04/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.ausiyevich.lesson04;
 
+import java.util.Scanner;
+
 /*
 Таблица умножения
 Вывести на экран таблицу умножения size х size используя цикл while.
@@ -24,5 +26,20 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
-
+    public static void main(String[] args) throws Exception
+    {
+        int i=1,n=1;
+        while(i<=10)
+        {
+            System.out.print(i+" ");
+            while(n<=9)
+            {
+                n++;
+                System.out.print(i*n+" ");
+            }
+            i++;
+            n=1;
+            System.out.println("");
+        }
+    }
 }
