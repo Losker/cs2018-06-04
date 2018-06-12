3e22a0db17d68dc63fa878282ca2302fdab5a99d
Ausiyevich <avsievich69@gmail.com>
2018-06-12 16:51:27 +0300

    Lesson4 TaskA completed

--- src/by/it/ausiyevich/lesson04/TaskA3.java
+++ src/by/it/ausiyevich/lesson04/TaskA3.java
@@ -1,4 +1,4 @@
-package by.it._tasks_.lesson04;
+package by.it.ausiyevich.lesson04;
 /*
 Существует ли пара?
 Ввести с клавиатуры три целых числа.
@@ -22,6 +22,22 @@ package by.it._tasks_.lesson04;
  */
 
 
-public class TaskA3 {
+import java.util.Scanner;
 
-}
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        if (a == b && b==c)
+            System.out.println(a+" "+b+" "+c);
+        else if (a==b)
+            System.out.println(a+" "+b);
+        else if(b==c)
+            System.out.println(b+" "+c);
+        else if (a==c)
+            System.out.println(a+" "+c);
+        }
+    }
+
