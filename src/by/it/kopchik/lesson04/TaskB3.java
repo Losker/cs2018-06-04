3ba43594274754a567c35767fc38d9685292acc0
Carol1975 <karolina.kopchik@gmail.com>
2018-06-13 13:43:44 +0300

    lesson 4 b

--- src/by/it/kopchik/lesson04/TaskB3.java
+++ src/by/it/kopchik/lesson04/TaskB3.java
@@ -25,7 +25,22 @@ package by.it.kopchik.lesson04;
  */
 
 
+import java.util.Scanner;
 
 public class TaskB3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int sum=1;
+
+
+        for (int i = 1; i < x; i++) {
+            i = 2;
+            i++ ;
+            System.out.print(sum+",");
+        }
+
+        }
 
 }
